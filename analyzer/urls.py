ce33bcfad61a4fe5e9139654dcef33c20bab9936
Yemel Jardi <yemel.jardi@bigdeal.com.ar>
2013-06-23 21:57:11 -0300

    Add static url for admin

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,3 +9,8 @@ urlpatterns = patterns('',
     url(r'^analyze/$', 'core.views.analyze'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
