cbe96904885af99e3137f85f042648b047e632ed
Yemel Jardi <yemel.jardi@bigdeal.com.ar>
2013-06-23 21:35:42 -0300

    Add keyword suggestions

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Keyword(models.Model):
+    keyword = models.CharField(unique=True, max_length=300)
+    hits = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.keyword
--- core/views.py
+++ core/views.py
@@ -1,19 +1,42 @@
 import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
 
 from core.engine import KeywordAnalyzer
+from core.models import Keyword
+
+
+class SearchForm(forms.Form):
+    keyword = forms.CharField()
+    language = forms.CharField()
+
+    def clean_keyword(self):
+        return self.cleaned_data['keyword'].lower()
+
+    def process(self):
+        keyword = self.cleaned_data['keyword']
+        language = self.cleaned_data['language']
+
+        search = Keyword.objects.filter(keyword=keyword)
+        search = search[0] if search.exists() else Keyword.objects.create(keyword=keyword, hits=0)
+
+        search.hits += 1
+        search.save()
+
+        return KeywordAnalyzer(keyword, language).run()
 
 
 def home(request):
-    return render(request, 'index.html')
+    ideas = Keyword.objects.order_by('hits')[:7]
+    return render(request, 'index.html', {'ideas': ideas})
 
 
 def analyze(request):
-    keyword = request.POST['keyword']
-    language = request.POST['language']
+    form = SearchForm(request.POST)
+    if form.is_valid():
+        analyzer = form.process()
+        response = json.dumps(analyzer.suggestions)
+        return HttpResponse(response, content_type="application/json")
 
-    analyzer = KeywordAnalyzer(keyword, language).run()
-
-    response = json.dumps(analyzer.suggestions)
-    return HttpResponse(response, content_type="application/json")
+    return HttpResponse("[]", content_type="application/json")
